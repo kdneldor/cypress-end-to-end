1562c3d611d88af2f647a64fca926bb82e2d9fc1
GreenKnightGoof <60906118+GreenKnightGoof@users.noreply.github.com>
2020-07-15 20:35:36 -0500

    Got a lot done

---
+++ cypress/integration/first_test.js
@@ -0,0 +1,5 @@
+describe('our first test', () => {
+    it ('is just a dummy test', () => {
+        expect(true).to.equal(true)
+    })
+})
---
+++ cypress/integration/form_test.js
@@ -0,0 +1,23 @@
+describe("form test", () => {
+  it("tests that the form is working", () => {
+    cy.visit("http://localhost:3000/");
+
+    cy.get("button").should("be.disabled");
+
+    cy.get('[for="name"] > input')
+      .type("Kyle Nelson")
+      .should("have.value", "Kyle Nelson");
+
+    const email = "kyle.nelson@lambdaschool.com";
+    cy.get('[for="email"] > input').type(email).should("have.value", email);
+
+    const motivation = "I want to help others in the community";
+    cy.get("textarea").type(motivation).should("have.value", motivation);
+
+    cy.get("#positions").select("Yard Work").should("have.value", "Yard Work");
+
+    cy.get(".terms > input").click().should('have.checked', true);
+
+    cy.get("button").should("not.be.disabled");
+  });
+});
